ee56252643780d6a957d22a710d67bd6984f9036
tushar-glitch <tusharc20001@gmail.com>
2023-01-14 14:15:27 +0530

    Add pitch api added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const userroutes = require('./routes/userroutes')
+const pitchroute = require('./routes/pitchroute')
 const app = express()
 app.use(express.json())
 app.use(cors())
@@ -11,6 +12,7 @@ const db = mongoose.connection
 db.on('open', () => {
     console.log('Database connected successfully!');
 })
-app.use('/',userroutes)
+app.use('/', userroutes)
+app.use('/pitch',pitchroute)
 const PORT = process.env.port||3001
 app.listen(PORT)
